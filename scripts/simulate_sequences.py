cdafad4837a9369dacfea90c7c2556c6e7b59b56
Bernhard Lang <bernhard.lang@gmx.de>
2015-02-25 11:24:46 +0100

    Haplotype simulation shall create a data tar.gz archive

--- scripts/simulate_sequences.py
+++ scripts/simulate_sequences.py
@@ -4,6 +4,8 @@ from subprocess import call
 import random
 import re
 import argparse
+import tarfile
+import tempfile
 
 class Sequence:
 
@@ -14,6 +16,12 @@ class Sequence:
         self.bases = ['A', 'G', 'C', 'T']
 
         self.base_freqs = {'A': 0, 'G': 0, 'C': 0, 'T': 0}
+
+    def replicate(self):
+        new_seq = Sequence(self.identifier, self.description)
+        new_seq.add_sequence(''.join(self.sequence))
+
+        return new_seq
 
     def add_sequence(self, seq):
         old_len = len(self.sequence)
@@ -71,16 +79,15 @@ class Sequence:
 
         self.sequence[pos:pos+length] = []
 
-    def writeFASTA(filename):
-        with open(filename, 'w') as f:
-            f.write('>' + self.identifier + '|' + self.description + '\n')
-            ct = 0
-            for base in self.sequence:
-                f.write(base)
-                ct += 1
-                if ct == 80:
-                    ct = 0
-                    f.write('\n')
+    def writeFASTA(fileobj):
+        fileobj.write('>' + self.identifier + '|' + self.description + '\n')
+        ct = 0
+        for base in self.sequence:
+            fileobj.write(base)
+            ct += 1
+            if ct == 80:
+                ct = 0
+                fileobj.write('\n')
 
 class ParsingError(Exception):
     def __init__(self, message):
@@ -105,7 +112,67 @@ def readFASTA(filename):
     return seq
 
 def main(argv):
-    pass
+    parser = argparse.ArgumentParser(description='Generate haplotypes from a reference genome and create simulated illumina reads with SimSeq')
+    parser.add_argument('-i', '--input', help='the reference genome in FASTA format (REQUIRED)', metavar='reference.fasta')
+    parser.add_argument('-o', '--output', help='destination of the generated data tar.gz archive. It contains the reference sequence, the generated haplotypes and the simulated sam file.', metavar='output.tar.gz')
+    parser.add_argument('--snp', help='generate new haplotypes through substituting random nucleotides.')
+    parser.add_argument('--ins', help='generate new haplotypes through inserting random sequences.')
+    parser.add_argument('--del', help='generate new haplotypes through deleting random sequences.')
+    parser.add_argument('-m', '--mean', type=int, help='mean value for length of indels or number of nucleotide substitutions')
+    parser.add_argument('-s', '--sigma', type=int, help='standard deviation for length of indels or number of nucleotide substitions')
+    parser.add_argument('-n', '--number', type=int, help='number of created haplotypes')
+    parser.add_argument('--seed', type=int, help='seed for the Random Number Generator')
+
+    args = parser.parse_args(argv)
+
+    ref_genome = readFASTA(args.input)
+
+    m = re.match('(?P<name>.*)(\.fasta$)', args.input)
+    filename = m.group('name')
+
+    if not args.output:
+        args.output = filename + '_sht.tar.gz'
+
+    tar = tarfile.open(args.output, 'w:gz')
+    tar.add(args.input, arcname='ref_' + args.input)
+
+    n = args.n
+
+    haplofiles = []
+
+    for i in range(0, n):
+        seq = ref_genome.replicate()
+
+        mean = args.mean
+        sigma = args.sigma
+
+        if mean < 1:
+            mean *= len(ref_genome.sequence)
+            sigma *= len(ref_genome.sigma)
+
+        if args.snp:
+            for i in range(0, random.gauss(mean, sigma))
+                seq.create_snp()
+
+        if args.ins:
+            seq.create_insertion(mean, sigma)
+
+        if args.del:
+            seq.create_deletion(mean, sigma)
+
+        f = tempfile.NamedTemporaryFile()
+        seq.writeFASTA(f)
+        info = tar.gettarinfo(arcname = 'ht' + i + '_' + filename + '.fasta',fileobj=f)
+        tar.addfile(info, f)
+
+        haplofiles.append(f)
+
+    for hf in haplofiles:
+
+        call(['../bin/SimSeq.jar', #TODO 
+        hf.close()
+
+    tar.close()
 
 if __name__ == '__main__':
     main(sys.argv)
