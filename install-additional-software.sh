2afeee3575b6da57b189eed4ca6054757250c7c6
David Seifert <soap@soap.za.net>
2015-07-23 10:51:17 +0200

    Remove superfluous whitespace

--- install-additional-software.sh
+++ install-additional-software.sh
@@ -4,9 +4,9 @@ cd bwa
 make
 cd ..
 
-for sw in htslib bcftools samtools; do                                                                                                                              
-  git clone --branch=develop git://github.com/samtools/${sw}.git                                                                                                    
-  cd ${sw}                                                                                                                                                          
-  make                                                                                                                                                              
-  cd ..                                                                                                                                                             
-done                                                                                                                                                                
+for sw in htslib bcftools samtools; do
+	git clone --branch=develop git://github.com/samtools/${sw}.git
+	cd ${sw}
+	make
+	cd ..
+done
