23dca526ed7e5e3243b0d78f973b01d694fb6cdb
MaryamZaheri <zaherim@bs-mbpr148.d.ethz.ch>
2017-03-13 15:27:14 +0100

    More tests for edgeBetween function

--- test/unit/unit2.cpp
+++ test/unit/unit2.cpp
@@ -1,26 +1,24 @@
 #include "gtest/gtest.h"
 
 #include <algorithm>
-//#include <deque>
-//#include <vector>
 #include <unordered_map>
 
 #include <boost/algorithm/string.hpp>
 
 #include "AlignmentRecord.h"
 #include "NewEdgeCalculator.h"
-//#include <unistd.h>
+
 
 using namespace std;
 using namespace boost;
 
-// This test verifies if the algorithm considers an edge between twp identical AlignmentRecords.
-TEST(edgeBetweenFunctionTest, edgeBetweenSameAlignmentRecord){
+// This test verifies if the algorithm considers an edge between two identical AlignmentRecords.
+TEST(edgeBetweenFunctionTest, edgeBetweenSameAlignments){
     
     AlignmentRecord alignment;
     EdgeCalculator* edge_calculator = nullptr;
     
-    alignment.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample.txt");
+    alignment.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample00.txt");
     
     double Q = 0.9;
     double edge_quasi_cutoff_cliques = 0.99;
@@ -42,4 +40,79 @@ TEST(edgeBetweenFunctionTest, edgeBetweenSameAlignmentRecord){
     delete edge_calculator;
 }
 
+// This test verifies if the algorithm considers an edge between two non_overlapping AlignmentRecords.
+TEST(edgeBetweenFunctionTest, edgeBetweenNonOverlappingAlignments){
+    
+    AlignmentRecord alignment1;
+    AlignmentRecord alignment2;
+    EdgeCalculator* edge_calculator = nullptr;
+    try{
+        alignment1.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample01.txt");
+    }catch (...){
+        cout << "File not found!" << endl;
+    }
+    
+    try{
+        alignment2.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample02.txt");
+    }catch (...){
+        cout << "File 2 not found!" << endl;
+    }
+    
+    double Q = 0.9;
+    double edge_quasi_cutoff_cliques = 0.99;
+    double overlap_cliques = 0.9;
+    bool frameshift_merge = false;
+    std::unordered_map<int, double> simpson_map;
+    double edge_quasi_cutoff_single = 0.95;
+    double overlap_single = 0.6;
+    double edge_quasi_cutoff_mixed = 0.97;
+    unsigned int maxPosition1 = 0;
+    bool noProb0 = false;
+    
+    edge_calculator = new NewEdgeCalculator(Q, edge_quasi_cutoff_cliques, overlap_cliques, frameshift_merge, simpson_map, edge_quasi_cutoff_single, overlap_single, edge_quasi_cutoff_mixed, maxPosition1, noProb0);
+    
+    bool set_edge = edge_calculator->edgeBetween(alignment1, alignment2);
+    
+    EXPECT_EQ(set_edge, false);
+    
+    delete edge_calculator;
+}
 
+// This test verifies if the algorithm considers an edge between two partially overlapping AlignmentRecords.
+// The second pair of the first read is aligned with the first pair of second read.
+TEST(edgeBetweenFunctionTest, edgeBetweenOverlappingCrossPairsAlignments){
+    
+    AlignmentRecord alignment1;
+    AlignmentRecord alignment2;
+    EdgeCalculator* edge_calculator = nullptr;
+    try{
+        alignment1.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample00.txt");
+    }catch (...){
+        cout << "File not found!" << endl;
+    }
+    
+    try{
+        alignment2.restoreAlignmentRecord("test/data/simulation/unit_data/alignment_sample03.txt");
+    }catch (...){
+        cout << "File 2 not found!" << endl;
+    }
+    
+    double Q = 0.9;
+    double edge_quasi_cutoff_cliques = 0.99;
+    double overlap_cliques = 0.9;
+    bool frameshift_merge = false;
+    std::unordered_map<int, double> simpson_map;
+    double edge_quasi_cutoff_single = 0.95;
+    double overlap_single = 0.6;
+    double edge_quasi_cutoff_mixed = 0.97;
+    unsigned int maxPosition1 = 0;
+    bool noProb0 = false;
+    
+    edge_calculator = new NewEdgeCalculator(Q, edge_quasi_cutoff_cliques, overlap_cliques, frameshift_merge, simpson_map, edge_quasi_cutoff_single, overlap_single, edge_quasi_cutoff_mixed, maxPosition1, noProb0);
+    
+    bool set_edge = edge_calculator->edgeBetween(alignment1, alignment2);
+    
+    EXPECT_EQ(set_edge, false);
+    
+    delete edge_calculator;
+}
